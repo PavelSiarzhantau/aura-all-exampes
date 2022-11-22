9ea5c36d4ea1292e0b23451d425b041dafc17d0c
Pavel Siarzhantau <pavel.siarzhantau@gmail.com>
2022-11-22 12:08:16 +0100

    feat: add addition resources

--- force-app/main/default/aura/appendChildToBody/appendChildToBodyController.js
+++ force-app/main/default/aura/appendChildToBody/appendChildToBodyController.js
@@ -1,23 +1,22 @@
 /* eslint-disable no-console */
 ({
-    handleClick : function(component, event, helper) {
+    handleClick: function (component, event, helper) {
         $A.createComponent(
-            "lightning:card",{"title" : "Dynamic Component"},
-             function(card, status, errorMessages){
-                 if (status === "SUCCESS") {
-                     var dynamicCards = component.get("v.dynamicCards");
-                     dynamicCards.push(card);
-                     component.set("v.dynamicCards", dynamicCards);
+            "lightning:card",
+            { title: "Dynamic Component" },
+            function (card, status, errorMessages) {
+                if (status === "SUCCESS") {
+                    var dynamicCards = component.get("v.dynamicCards");
+                    dynamicCards.push(card);
+                    component.set("v.dynamicCards", dynamicCards);
                     //  console.log('fired');
                     // var body = component.get("v.body");
                     // body.push(card);
                     // component.set("v.body", card);
-                }
-                else if (status === "INCOMPLETE") {
-                    console.log("No response from server or client is offline.")
+                } else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.");
                     // Show offline error
-                }
-                else if (status === "ERROR") {
+                } else if (status === "ERROR") {
                     console.log("Error message: " + errorMessages[0].message);
                 }
             }
@@ -25,58 +24,54 @@
     },
     replaceBody: function (component, event, helper) {
         var allCards = component.get("v.dynamicCards");
-        allCards.forEach(e => {
+        allCards.forEach((e) => {
             $A.createComponent(
                 "lightning:button",
                 {
                     "aura:id": "findableAuraId",
-                    "label": "Press Me",
+                    label: "Press Me"
                 },
-                function(newButton, status, errorMessage){
+                function (newButton, status, errorMessage) {
                     //Add the new button to the body array
                     if (status === "SUCCESS") {
                         var body = e.get("v.body");
                         e.set("v.body", newButton);
-                    }
-                    else if (status === "INCOMPLETE") {
-                        console.log("No response from server or client is offline.")
+                    } else if (status === "INCOMPLETE") {
+                        console.log("No response from server or client is offline.");
                         // Show offline error
-                    }
-                    else if (status === "ERROR") {
+                    } else if (status === "ERROR") {
                         console.log("Error: " + errorMessage);
                         // Show error message
                     }
                 }
             );
-        })
+        });
         component.set("v.dynamicCards", allCards);
-        
     },
     createDiv: function (component, event, helper) {
-        $A.createComponent("aura:HTML",
+        $A.createComponent(
+            "aura:HTML",
             {
-                "tag": "div",
-                "body":"Some Text",
-                "HTMLAttributes": { "id": "myDiv" },
-                "class": "",
+                tag: "div",
+                body: "Some Text",
+                HTMLAttributes: { id: "myDiv" },
+                class: ""
             },
             function (result, status, errorMessage) {
                 if (status === "SUCCESS") {
-                    console.log('SUCCESS createDiv()');
-                    
+                    console.log("SUCCESS createDiv()");
+
                     var body = component.get("v.body");
-                    body.unshift(newCmp);
                     body.push(result);
                     component.set("v.body", result);
-                }else if (status === "INCOMPLETE") {
-                    console.log("No response from server or client is offline.")
+                } else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.");
                     // Show offline error
-                }
-                else if (status === "ERROR") {
+                } else if (status === "ERROR") {
                     console.log("Error: " + errorMessage);
                     // Show error message
                 }
             }
-        )
+        );
     }
-})
+});
--- force-app/main/default/aura/harnessApp/harnessApp.app
+++ force-app/main/default/aura/harnessApp/harnessApp.app
@@ -1,3 +1,4 @@
 <aura:application extends="force:slds">
+    <c:myFirstComponent/>
     <c:camping/>
 </aura:application>	
